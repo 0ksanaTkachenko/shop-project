c40e3e7256ce1252e63d613211f4d1ea7836a2f8
Oksana <ksenia.tkachenko.ua@gmail.com>
2022-12-21 20:36:38 -0500

    add item counter

--- app.js
+++ app.js
@@ -74,3 +74,59 @@ function openModalScroll() {
     window.removeEventListener("scroll", openModalScroll);
   }
 }
+
+//  Product quantity counter
+
+let decrementBtns = document.querySelectorAll(".decrement-btn");
+let incrementBtns = document.querySelectorAll(".increment-btn");
+let inputFields = document.querySelectorAll(".product-quantity input");
+
+function Counter(incrementBtn, decrementBtn, inputField) {
+  this.domRefs = {
+    incrementBtn,
+    decrementBtn,
+    inputField,
+  };
+
+  this.toggleButtonState = function () {
+    let count = this.domRefs.inputField.value;
+    this.domRefs.decrementBtn.disabled = count <= 1;
+    this.domRefs.incrementBtn.disabled = count >= 10;
+  };
+
+  this.toggleButtonState();
+
+  this.increment = function () {
+    this.domRefs.inputField.value = +this.domRefs.inputField.value + 1;
+    this.toggleButtonState();
+  };
+
+  this.decrement = function () {
+    this.domRefs.inputField.value = +this.domRefs.inputField.value - 1;
+    this.toggleButtonState();
+  };
+
+  this.domRefs.incrementBtn.addEventListener(
+    "click",
+    this.increment.bind(this)
+  );
+  this.domRefs.decrementBtn.addEventListener(
+    "click",
+    this.decrement.bind(this)
+  );
+}
+
+//  Варiант 1
+// let counter = new Counter(incrementBtns[0], decrementBtns[0], inputFields[0]);
+// let counter = new Counter(incrementBtns[1], decrementBtns[2], inputFields[3]);
+// й так для всiх товарiв
+
+// Варiант 2
+
+for (i = 0; i <= decrementBtns.length; i++) {
+  let decrementBtn = decrementBtns[i];
+  let incrementBtn = incrementBtns[i];
+  let inputField = inputFields[i];
+
+  new Counter(incrementBtn, decrementBtn, inputField);
+}
